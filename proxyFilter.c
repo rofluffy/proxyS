e023a29fb50598b1ce8083ba3397392b3ec27e64
fluffy(j7n8) <shirley5001@hotmail.com>
2015-03-24 15:17:21 -0700

    Added check port method but contain mysterious seg fault

--- proxyFilter.c
+++ proxyFilter.c
@@ -26,8 +26,6 @@ int main(int argc, char **argv) {
   int sd, new_sd, client_len, port;
   struct sockaddr_in server, client;
   char *bp, buf[BUFLEN], outbuf[BUFLEN];
-  
-  struct hostent* host;
 
   // running start up (have 2 argument, so one more to implement)
   /*if (argc != 3){
@@ -70,19 +68,6 @@ int main(int argc, char **argv) {
 
     bp = buf;
     bytes_to_read = BUFLEN;
-	char *checker = NULL;
-	char *http = NULL;
-	char *path = NULL;
-	char *ver = NULL;
-	
-	struct sockaddr_in host_addr;
-	int hostsd, new_hostsd, i; 
-	
-	char request[BUFLEN];
-	char response[BUFLEN];
-	char *rp;
-	char respondCode[4];
-	char *token;
 	
 
     while((n = read(new_sd, bp, bytes_to_read)) > 0) {
@@ -90,14 +75,32 @@ int main(int argc, char **argv) {
         break;
       }
 	  
+	  char *checker = NULL;
+	  char *http = NULL;
+	  char *path = NULL;
+	  char *ver = NULL;
+	  char *flag = NULL;
+	  char *port = NULL;
+	
+	  struct sockaddr_in host_addr;
+	  int hostsd, new_hostsd, i; 
+	
+	  char request[BUFLEN];
+	  char response[BUFLEN];
+	  char *token;
+	  struct hostent* host;
+	  int portNum = DEFAULT_PORT;
+	  
 	  // get pointer to HTTPver
 	  ver = strstr(bp, "HTTP/1.1");
  
-	  checker = strstr(bp, "GET"); 
+	  checker = strstr(bp, "GET");
+
+	  http = strstr(bp, "http://");
 	  
 	  if ((checker == bp) && ver != NULL){
 		   // get the pointer to http://
-		  http = strstr(bp, "http://");
+		  //http = strstr(bp, "http://");
 		  
 		  // get the pointer to absPath
 		  http += 7;
@@ -109,16 +112,29 @@ int main(int argc, char **argv) {
 		  
 		  // get the host
 		  // if port exist, then extract it
-		  int hlen = (path - http) + 1;
+		  int hlen = (path - http)+1;
 		  char *hostname = malloc(hlen * sizeof(char));
+		  //bzero((char*) hostname, sizeof(hostname));
+		  printf("check hlen: %d\n", hlen);
 		  strncpy(hostname, http, hlen-1);
-		  hostname[hlen] = 0;
+		  flag = strstr(hostname, ":");
+		  if (flag != NULL){
+			  int portlen = hostname + hlen - flag;
+			  port = malloc(portlen * sizeof(char));
+			  strncpy(port, flag+1, portlen);
+			  if (portlen > 2){
+				  portNum = atoi(port);
+			  }
+			  hostname = strtok(hostname, ":");
+		  }
+		  hostname[strlen(hostname)] = 0;
 		  host = gethostbyname(hostname);
 		  
 		  // get the absPath
 		  // if it's empty, then should be "/"
 		  int plen = (ver - path);
 		  char *absPath = malloc(plen * sizeof(char));
+		  //bzero((char*) absPath, sizeof(absPath));
 		  strncpy(absPath, path, plen);
 		  absPath[plen] = 0;
 		  if (absPath[0] == ' '){
@@ -128,25 +144,30 @@ int main(int argc, char **argv) {
 		  // get the HTTPver
 		  int vlen = strrchr(bp, '\0') - ver - 1;
 		  char *httpVer = malloc(vlen * sizeof(char));
+		  //bzero((char*) httpVer, sizeof(httpVer));
 		  strcpy(httpVer, ver);
 		  httpVer[vlen] = 0;
-		  
-		  
+
 		  printf("GET %s %s\nHOST: %s\n", absPath, httpVer, hostname);
 		  
 		  // Check statements
 		  /*printf("check host: %s\n", host);
 		  printf("check absPath: %s\n", absPath);
 		  printf("check HTTPver: %s\n", httpVer);*/
-		  
+		  //printf("check (0)");
 		  // create new socket on HOST, [port]
 		  /* Bind host address to the socket */
-		  bzero((char*) &host_addr, sizeof(host_addr));
 		  memset((char *) &host_addr, 0, sizeof(struct sockaddr_in));
+		  //printf("check (1)");
+		  bzero((char*) &host_addr, sizeof(host_addr));
+		  //printf("check (2)");
 		  host_addr.sin_family = AF_INET;
+		  //printf("check (3)");
 		  // if port exist, use that instead of default
-		  host_addr.sin_port = htons(DEFAULT_PORT);  
+		  host_addr.sin_port = htons(portNum);  
+		  //printf("check (4)");
 		  bcopy((char*)host->h_addr,(char*)&host_addr.sin_addr.s_addr,host->h_length);
+		  //printf("check (5)");
 		  //host_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 		  
 		  /* Create a stream socket. */
@@ -163,7 +184,11 @@ int main(int argc, char **argv) {
 		  // send "GET absPath httpver" request to the socket
 		  //sprintf(request, "GET %s %s\r\nHOST: %s\r\nConnection: close\r\n\r\n", absPath, httpVer, hostname);
 		  bzero((char*)request,sizeof(request));
-		  sprintf(request, "GET %s %s\r\nHOST: %s\r\n\r\n", absPath, httpVer, hostname);
+		  if (port != NULL){
+			  sprintf(request, "GET %s %s\r\nHOST: %s:%s\r\n\r\n", absPath, httpVer, hostname, port);
+		  }else{
+			  sprintf(request, "GET %s %s\r\nHOST: %s\r\n\r\n", absPath, httpVer, hostname);
+		  }
 		  i = send(hostsd, request, strlen(request), 0);
 		  printf("\n%s\n", request);
 		  
@@ -197,13 +222,20 @@ int main(int argc, char **argv) {
 		  //fprintf(stdout, "\nResponse: %s\n", response);
 		  
 		  // clean up
+		  bzero((char*) hostname, sizeof(hostname));
+		  bzero((char*) absPath, sizeof(absPath));
+		  bzero((char*) httpVer, sizeof(httpVer));
 		  free(hostname);
 		  free(absPath);
 		  free(httpVer);
+		  free(port);
+		  printf("clean up freeing memory.");
 		  
 		  // close connection
 		  close(hostsd);
 		  close(new_hostsd);
+		  close(new_sd);
+		  printf("-------Client closing-------\n\n");
 		  
 	  } else {
 		 
@@ -226,8 +258,8 @@ int main(int argc, char **argv) {
     printf("Sent: %s\n", outbuf);
 
     /* Clean up. */
-    close(new_sd);
-	printf("after close new_sd\n");
+    //close(new_sd);
+	//printf("after close new_sd\n");
   }
 
   close(sd);
