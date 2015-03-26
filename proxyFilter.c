9d5c056e5c640d3c9d6fa53e2d37ef452debe5e4
fluffy(j7n8) <shirley5001@hotmail.com>
2015-03-26 13:30:19 -0700

    extracted function

--- proxyFilter.c
+++ proxyFilter.c
@@ -19,76 +19,9 @@ char* str_to_lower(char* str){
 	return str;
 }
 
-int main(int argc, char **argv) {
-
-    // This is some sample code feel free to delete it
-
-    /*int i;
-
-    for (i = 0; i < argc; i++) {
-        printf("Arg %d is: %s\n", i, argv[i]);
-    }
-
-    return 0;*/
+void whatever(char* bp, char* ver, char* http, int new_sd){
 	
-  int n, bytes_to_read;
-  int sd, new_sd, client_len, port;
-  struct sockaddr_in server, client;
-  char *bp, buf[BUFLEN], outbuf[BUFLEN];
-  
-  char* err_msg;
-
-  // running start up (have 2 argument, so one more to implement)
-  /*if (argc != 3){
-	  fprintf(stderr, "Usage: %s [port] %s [blacklist]\n", argv[0]);
-      exit(1);
-  }*/
-  if (argc != 2){
-	  fprintf(stderr, "Usage: %s [port]\n", argv[0]);
-      exit(1);
-  }
-  port = atoi(argv[1]);
-
-  /* Create a stream socket. */
-  if ((sd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-    fprintf(stderr, "Can't create a socket.\n");
-    exit(1);
-  }
-
-  /* Bind an address to the socket */
-  memset((char *) &server, 0, sizeof(struct sockaddr_in));
-  server.sin_family = AF_INET;
-  server.sin_port = htons(port);
-  server.sin_addr.s_addr = htonl(INADDR_ANY);
-  if (bind(sd, (struct sockaddr *)&server, sizeof(server)) ==-1) {
-    fprintf(stderr, "Can't bind name to socket.\n");
-    exit(1);
-  }
-
-  /* Receive from the client. */
-  listen(sd, 5);
-  while (1) {
-    memset(buf, 0, sizeof(buf));
-    if ((new_sd = accept(sd, (struct sockaddr *)&client, &client_len)) == -1) {
-      fprintf(stderr, "Can't accept client.\n");
-      exit(1);
-    }
-	
-	printf("\n-------Reserve connection-------\n\n");
-
-    bp = buf;
-    bytes_to_read = BUFLEN;
-	
-
-    while((n = read(new_sd, bp, bytes_to_read)) > 0) {
-      if (*bp == '\n') {
-        break;
-      }
-	  
-	  char *checker = NULL;
-	  char *http = NULL;
-	  char *path = NULL;
-	  char *ver = NULL;
+	char *path = NULL;
 	  char *flag = NULL;
 	  
 	  char *hostname = NULL;
@@ -104,16 +37,8 @@ int main(int argc, char **argv) {
 	  char *token;
 	  struct hostent* host;
 	  int portNum = DEFAULT_PORT;
-	  
-	  // get pointer to HTTPver
-	  ver = strstr(bp, "HTTP/1.1");
- 
-	  checker = strstr(bp, "GET");
-
-	  http = strstr(bp, "http://");
-	  
-	  if ((checker == bp) && ver != NULL){
-		   // get the pointer to http://
+	
+	// get the pointer to http://
 		  //http = strstr(bp, "http://");
 		  
 		  // get the pointer to absPath
@@ -247,6 +172,113 @@ int main(int argc, char **argv) {
 		  close(new_hostsd);
 		  close(new_sd);
 		  printf("\n-------Closing client-------\n\n\n");
+}
+
+int main(int argc, char **argv) {
+
+    // This is some sample code feel free to delete it
+
+    /*int i;
+
+    for (i = 0; i < argc; i++) {
+        printf("Arg %d is: %s\n", i, argv[i]);
+    }
+
+    return 0;*/
+	
+  int n, bytes_to_read;
+  int sd, new_sd, client_len, port;
+  struct sockaddr_in server, client;
+  char *bp, buf[BUFLEN], outbuf[BUFLEN];
+  
+  char* err_msg;
+
+  // running start up (have 2 argument, so one more to implement)
+  /*if (argc != 3){
+	  fprintf(stderr, "Usage: %s [port] %s [blacklist]\n", argv[0]);
+      exit(1);
+  }*/
+  if (argc != 2){
+	  fprintf(stderr, "Usage: %s [port]\n", argv[0]);
+      exit(1);
+  }
+  port = atoi(argv[1]);
+
+  /* Create a stream socket. */
+  if ((sd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+    fprintf(stderr, "Can't create a socket.\n");
+    exit(1);
+  }
+
+  /* Bind an address to the socket */
+  memset((char *) &server, 0, sizeof(struct sockaddr_in));
+  server.sin_family = AF_INET;
+  server.sin_port = htons(port);
+  server.sin_addr.s_addr = htonl(INADDR_ANY);
+  if (bind(sd, (struct sockaddr *)&server, sizeof(server)) ==-1) {
+    fprintf(stderr, "Can't bind name to socket.\n");
+    exit(1);
+  }
+
+  /* Receive from the client. */
+  listen(sd, 5);
+  while (1) {
+    memset(buf, 0, sizeof(buf));
+    if ((new_sd = accept(sd, (struct sockaddr *)&client, &client_len)) == -1) {
+      fprintf(stderr, "Can't accept client.\n");
+      exit(1);
+    }
+	
+	printf("\n-------Reserve connection-------\n\n");
+
+    bp = buf;
+    bytes_to_read = BUFLEN;
+	
+
+    while((n = read(new_sd, bp, bytes_to_read)) > 0) {
+      if (*bp == '\n') {
+        break;
+      }
+	  
+	  char *checker = NULL;
+	  char *http = NULL;
+	  char *ver = NULL;
+	  
+	  /*char *path = NULL;
+	  char *flag = NULL;
+	  
+	  char *hostname = NULL;
+	  char *absPath = NULL;
+	  char *httpVer = NULL;
+	  char *hostport = NULL;
+	
+	  struct sockaddr_in host_addr;
+	  int hostsd, new_hostsd, i; 
+	
+	  char request[BUFLEN];
+	  char response[BUFLEN];
+	  char *token;
+	  struct hostent* host;
+	  int portNum = DEFAULT_PORT;*/
+	  
+	  // get pointer to HTTPver
+	  ver = strstr(bp, "HTTP/1.1");
+ 
+	  checker = strstr(bp, "GET");
+
+	  http = strstr(bp, "http://");
+	  
+	  /*if (strncmp(bp, "GET", 3) != 0){
+		  printf("whatever");
+	  }else if (ver == NULL){
+		  printf("so what");
+	  }else if (http != NULL){
+		  printf("fine...")
+	  }*/
+	  
+	  if ((checker == bp) && ver != NULL){
+		   
+		   whatever(bp, ver, http, new_sd);
 		  
 	  } else {
 		 
